df97a4384ea217f98d05d4ee630d62c784ded6b9
gatobot123 <gatobot107@gmail.com>
2025-04-24 09:21:11 -0500

    github.com/GataNina-Li/GataBotLite-MD

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ global.botNumberCode = "" //example: "+59309090909"
 global.confirmCode = "" // No tocar esto : Do not touch this line
 
 // ES ➜ Agregue el código de idioma el cual usará GataBot  
-// EN ➜ Add the language code which GataBot will use
+// EN ➜ Add the language co qede which GataBot will use
 //  es = Español      id = Bahasa Indonesia       ar = عرب
 //  en = English      pt = Português              de = Deutsch
 //  it = Italiano
